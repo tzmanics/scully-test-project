bd51d5b186c27e2ac38109fc5f496df80fca947d
tzmanics <tzmanics@gmail.com>
2020-05-01 21:32:08 -0400

    adds scully routes and ntl init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ScullyRoutesService } from '@scullyio/ng-lib';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'scully-test-project';
+  constructor(public scullyRoutes: ScullyRoutesService) {}
 }
